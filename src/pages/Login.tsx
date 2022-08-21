5a388dc625805ee232d5cb4c7dd03144f4b14838
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-21 15:47:50 -0300

    styles Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Container, TextField, Button } from '@mui/material';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 function Login() {
   const [name, setValue] = useState('');
@@ -9,20 +13,39 @@ function Login() {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <TextField
-        label="Digite seu nome"
-        name="name"
-        value={name}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        disabled={name.length < 3}
-        onClick={() => { navigate('user/search'); }}
-      >
-        Entrar
-      </Button>
+    <Container sx={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -75%)',
+    }}
+    >
+      <Typography component="h1" variant="h3" align="center">
+        Trybetunes
+      </Typography>
+      <Container sx={{ marginTop: '50px', width: '70%' }}>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <TextField
+              label="Digite seu nome"
+              name="name"
+              value={name}
+              onChange={(e) => setValue(e.target.value)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              disabled={name.length < 3}
+              onClick={() => { navigate('user/search'); }}
+              fullWidth
+            >
+              Entrar
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </Container>
   );
 }
