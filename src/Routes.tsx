4a0c027d3be0dea9428fe6fa3c3659ab3a57421a
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-21 09:38:26 -0300

    feat: configs routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function Routes() {
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+
+function Router() {
   return (
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/album/:id" element={<Album />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/edit" element={<ProfileEdit />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default Routes;
+export default Router;
