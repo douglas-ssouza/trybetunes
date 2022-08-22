d3fd9f4ffcd4236d710c1792138c70390a683611
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-22 18:17:12 -0300

    feat: creates context Provider

---
+++ src/context/Provider.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode, useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import Context from './context';
+
+interface IProps {
+  children: ReactNode,
+}
+
+function Provider({ children }: IProps) {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+  });
+
+  const contextValue = useMemo(() => ({
+    user,
+    setUser,
+  }), [user]);
+
+  return (
+    <Context.Provider value={contextValue}>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
---
+++ src/context/context.ts
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+interface ContextValue {}
+
+const Context = createContext({} as ContextValue);
+
+export default Context;
