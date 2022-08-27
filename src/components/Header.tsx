560c2ac3b8bfb80de41d73fcfba746861133cf33
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-27 18:22:56 -0300

    feat: add mui icons lib

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { useTheme } from '@mui/material/styles';
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Tabs from '@mui/material/Tabs';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-import { StyledTabs, StyledTab } from './headerComponents';
+import StyledTab from './headerComponents';
 
 import ElevationScroll from './helpers/ElevationScroll';
 
 import { UserContext } from '../context/UserContext';
 
 function Header() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [tabValue, setTabValue] = useState(0);
+
+  const theme = useTheme();
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log(newValue);
+    setTabValue(newValue);
+  };
+
+  const handleLogout = () => {
+    setUser({
+      name: '', email: '', description: '', image: '',
+    });
+  };
 
   return (
     <>
       <ElevationScroll>
         <AppBar data-testid="page-header">
           <Toolbar>
-            <Typography component="h2" variant="h4">
+            <Typography component="h2" variant="h4" color={theme.palette.common.white}>
               {user.name}
             </Typography>
-            <StyledTabs>
+            <Tabs onChange={handleTabChange} sx={{ margin: 'auto' }}>
               <StyledTab label="Search" component={Link} to="/search" />
               <StyledTab label="Favorites" component={Link} to="/favorites" />
               <StyledTab label="Profile" component={Link} to="/profile" />
-            </StyledTabs>
+            </Tabs>
+            <IconButton component="button" disableRipple onClick={handleLogout}>
+              <LogoutIcon fontSize="large" />
+            </IconButton>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
--- src/components/headerComponents.tsx
+++ src/components/headerComponents.tsx
@@ -1,16 +1,11 @@
 import { styled } from '@mui/material/styles';
 
-import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
-export const StyledTabs = styled(Tabs)({
-  marginLeft: 'auto',
-});
-
-export const StyledTab = styled(Tab)(({ theme }) => ({
+const StyledTab = styled(Tab)(({ theme }) => ({
   color: 'white',
   fontWeight: 600,
-  marginLeft: '50px',
+  margin: '0 50px',
   minWidth: '100px',
   borderRadius: '7px',
   opacity: 0.7,
@@ -23,3 +18,5 @@ export const StyledTab = styled(Tab)(({ theme }) => ({
     backgroundColor: theme.palette.primary.light,
   },
 })) as typeof Tab;
+
+export default StyledTab;
