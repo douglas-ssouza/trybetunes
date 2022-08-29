5e4e449154ce8f902e0e4cf95546794e2f5c21ac
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-29 19:36:50 -0300

    feat: adds menu icon button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { useTheme } from '@mui/material/styles';
 
+import useMediaQuery from '@mui/material/useMediaQuery';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Tabs from '@mui/material/Tabs';
@@ -10,7 +11,10 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import LogoutIcon from '@mui/icons-material/Logout';
 
+import MenuIcon from '@mui/icons-material/Menu';
+
 import CustomTab from './CustomTab';
+import CustomMenu from './CustomMenu';
 
 import ElevationScroll from './helpers/ElevationScroll';
 
@@ -21,6 +25,7 @@ function Header() {
   const [tabValue, setTabValue] = useState(0);
 
   const theme = useTheme();
+  const isScreenSmall = useMediaQuery(theme.breakpoints.down('md'));
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
@@ -40,29 +45,41 @@ function Header() {
             <Typography component="h2" variant="h4" color={theme.palette.common.white}>
               {user.name}
             </Typography>
-            <Tabs value={tabValue} onChange={handleTabChange} sx={{ margin: 'auto' }}>
-              <CustomTab
-                label="Search"
-                selected={tabValue === 0}
-                component={Link}
-                to="/search"
-              />
-              <CustomTab
-                label="Favorites"
-                component={Link}
-                to="/favorites"
-                selected={tabValue === 1}
-              />
-              <CustomTab
-                label="Profile"
-                component={Link}
-                to="/profile"
-                selected={tabValue === 2}
-              />
-            </Tabs>
-            <IconButton component="button" disableRipple onClick={handleLogout}>
-              <LogoutIcon fontSize="large" />
-            </IconButton>
+            {
+              isScreenSmall
+                ? (
+                  <IconButton sx={{ ml: 'auto' }} disableRipple>
+                    <MenuIcon fontSize="large" />
+                  </IconButton>
+                )
+                : (
+                  <>
+                    <Tabs value={tabValue} onChange={handleTabChange} sx={{ margin: 'auto' }}>
+                      <CustomTab
+                        label="Search"
+                        selected={tabValue === 0}
+                        component={Link}
+                        to="/search"
+                      />
+                      <CustomTab
+                        label="Favorites"
+                        component={Link}
+                        to="/favorites"
+                        selected={tabValue === 1}
+                      />
+                      <CustomTab
+                        label="Profile"
+                        component={Link}
+                        to="/profile"
+                        selected={tabValue === 2}
+                      />
+                    </Tabs>
+                    <IconButton disableRipple onClick={handleLogout}>
+                      <LogoutIcon fontSize="large" />
+                    </IconButton>
+                  </>
+                )
+            }
           </Toolbar>
         </AppBar>
       </ElevationScroll>
