921a3f7201d32030d25ae28d9ef03d51e5f6ec4f
Douglas Souza <douglassouzabjj@hotmail.com>
2022-08-31 14:33:02 -0300

    removes drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 import { useTheme } from '@mui/material/styles';
 
-import useMediaQuery from '@mui/material/useMediaQuery';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-
-import MenuIcon from '@mui/icons-material/Menu';
 
 import HeaderTabs from './HeaderTabs';
 
@@ -24,10 +15,8 @@ import { UserContext } from '../context/UserContext';
 function Header() {
   const { user, setUser } = useContext(UserContext);
   const [tabValue, setTabValue] = useState(0);
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const theme = useTheme();
-  const isScreenSmall = useMediaQuery(theme.breakpoints.down('md'));
 
   useEffect(() => {
     const { pathname } = window.location;
@@ -59,8 +48,6 @@ function Header() {
     });
   };
 
-  const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
-
   return (
     <>
       <ElevationScroll>
@@ -69,50 +56,11 @@ function Header() {
             <Typography component="h2" variant="h4" color={theme.palette.common.white}>
               {user.name}
             </Typography>
-            {
-              isScreenSmall
-                ? (
-                  <IconButton
-                    sx={{ ml: 'auto' }}
-                    disableRipple
-                    onClick={() => setIsDrawerOpen(!isDrawerOpen)}
-                  >
-                    <MenuIcon fontSize="large" />
-                  </IconButton>
-                )
-                : (
-                  <HeaderTabs
-                    tabValue={tabValue}
-                    handleTabChange={handleTabChange}
-                    handleLogout={handleLogout}
-                  />
-                )
-            }
-            <SwipeableDrawer
-              open={isDrawerOpen}
-              onClose={() => setIsDrawerOpen(false)}
-              onOpen={() => setIsDrawerOpen(true)}
-              disableBackdropTransition={!iOS}
-              disableDiscovery={iOS}
-            >
-              <List>
-                <ListItem component={Link} to="/search" onClick={() => setTabValue(0)}>
-                  <ListItemText>
-                    Search
-                  </ListItemText>
-                </ListItem>
-                <ListItem component={Link} to="/favorites" onClick={() => setTabValue(1)}>
-                  <ListItemText>
-                    Favorites
-                  </ListItemText>
-                </ListItem>
-                <ListItem component={Link} to="/profile" onClick={() => setTabValue(2)}>
-                  <ListItemText>
-                    Profile
-                  </ListItemText>
-                </ListItem>
-              </List>
-            </SwipeableDrawer>
+            <HeaderTabs
+              tabValue={tabValue}
+              handleTabChange={handleTabChange}
+              handleLogout={handleLogout}
+            />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
