103ec5cf8b7fcc62154f609654d4b60229f713a6
Douglas Souza <douglassouzabjj@hotmail.com>
2022-09-07 11:31:51 -0300

    feat: adds edit profile page

--- src/pages/profileEdit/index.tsx
+++ src/pages/profileEdit/index.tsx
@@ -44,57 +44,57 @@ function EditProfile() {
   };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-    >
-      <Grid item>
+    <Grid container justifyContent="center" spacing={2}>
+      <Grid item xs={12} textAlign="center">
         {
           image
-            ? <img src={image} alt={`${name} profile`} />
+            ? <img src={image} alt={`${name} profile`} height="150px" />
             : <Box><AccountBoxIcon sx={{ fontSize: 150 }} /></Box>
         }
       </Grid>
-      <Grid item>
+      <Grid item xs={8}>
         <TextField
           placeholder="Digite seu nome"
           onChange={handleChange}
           name="name"
           value={state.name}
+          fullWidth
         />
       </Grid>
-      <Grid item>
+      <Grid item xs={8}>
         <TextField
           placeholder="Digite seu email"
           onChange={handleChange}
           name="email"
           value={state.email}
+          fullWidth
         />
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={8}>
         <TextField
           placeholder="Digite sua descrição "
           onChange={handleChange}
           name="description"
           value={state.description}
           multiline
-          maxRows={5}
+          rows={3}
           fullWidth
         />
       </Grid>
-      <Grid item>
+      <Grid item xs={8}>
         <TextField
           placeholder="Insira o caminho da imagem"
           onChange={handleChange}
           name="image"
           value={state.image}
+          fullWidth
         />
       </Grid>
-      <Grid item>
+      <Grid item xs={8}>
         <Button
           variant="contained"
           onClick={handleClick}
+          fullWidth
         >
           Salvar
         </Button>
